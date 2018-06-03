bba3c0db7a5af2ba2483a00de442204521a3721e
nevrome <clemens@nevrome.de>
2018-06-03 21:11:26 +0200

    replaced very simple simulation with the one that includes innovation

--- BART_2018_06_05/shiny_neiman_1.R
+++ BART_2018_06_05/shiny_neiman_1.R
@@ -3,20 +3,21 @@ library(ggplot2)
 library(magrittr)
 
 server <- function(input, output) {
-  drift_simulation_data <- eventReactive(
-    input$run_button_drift_simulation, {
+  drift_simulation_with_innovation_data <- eventReactive(
+    input$run_button_drift_simulation_with_innovation, {
       
       # read input
-      k <- input$k_drift_simulation
-      N <- input$Ne_drift_simulation
-      time <- input$t_drift_simulation
+      k <- input$k_drift_simulation_with_innovation
+      N <- input$Ne_drift_simulation_with_innovation
+      time <- input$t_drift_simulation_with_innovation
+      mu <- input$mu_drift_simulation_with_innovation
       
-      # calculate population parameters
+      # prepare population parameters
       population <- 1:N
       variants <- 1:k
       timesteps <- 2:time
       
-      # create initial population
+      # create starting population
       pop0 <- tibble::tibble(
         time = as.integer(0),
         individual = 1:N,
@@ -28,10 +29,23 @@ server <- function(input, output) {
       pop_devel[[1]] <- pop0
       
       # simulation loop
+      last_variant <- max(pop_devel[[1]]$variant)
       for (p1 in timesteps) {
-        pop_new <- pop_devel[[p1 - 1]] 
+        pop_new <- pop_devel[[p1 - 1]]
         pop_new$time <- p1 - 1
         pop_new$variant <- sample(pop_new$variant, length(pop_new$variant), replace = T)
+        
+        # innovation
+        innovate_here <- sample(
+          c(TRUE, FALSE), 
+          length(pop_new$variant), 
+          prob = c(mu, 1 - mu), 
+          replace = T
+        )
+        new_variants <- seq(last_variant + 1, last_variant + sum(innovate_here))
+        last_variant <- last_variant + sum(innovate_here)
+        pop_new$variant[innovate_here] <- new_variants
+        
         pop_devel[[p1]] <- pop_new
       }
       
@@ -47,7 +61,6 @@ server <- function(input, output) {
           individuals_with_variant = n()
         ) %>%
         dplyr::ungroup() %>%
-        # complete (expand.grid) to fill gaps in the area plot
         tidyr::complete(
           time, 
           variant, 
@@ -55,23 +68,19 @@ server <- function(input, output) {
         )
       
       pop_devel_sum
+      
     },
     ignoreNULL = FALSE
   )
   
-  output$simplot <- renderPlot({
-    
-    drift_simulation_data() %>%
+  output$simplot <-   renderPlot({
+    drift_simulation_with_innovation_data() %>%
       ggplot() +
       geom_area(aes(x = time, y = individuals_with_variant, fill = variant, group = variant)) +
       geom_line(aes(x = time, y = individuals_with_variant, group = variant), position = "stack") +
       theme_bw() +
-      theme(
-        plot.background = element_rect(fill = '#f1f1f1')
-      ) +
-      xlab(expression(paste("t"))) +
+      xlab("t") +
       ylab("variants and their occurence in the population [%]")
-    
   })
 }
 
@@ -85,13 +94,15 @@ ui <- fluidPage(
     )
   ),
   inputPanel(
-      sliderInput("k_drift_simulation", label = "k",
-                  min = 5, max = 15, value = 10, step = 1),
-      sliderInput("Ne_drift_simulation", label = "Ne",
-                  min = 10, max = 100, value = 20, step = 10),
-      sliderInput("t_drift_simulation", label = "t",
-                  min = 50, max = 200, value = 100, step = 50),
-      actionButton("run_button_drift_simulation", "Run simulation")
+    sliderInput("k_drift_simulation_with_innovation", label = "k for t == 0",
+                min = 5, max = 15, value = 10, step = 1),
+    sliderInput("Ne_drift_simulation_with_innovation", label = "Ne",
+                min = 10, max = 100, value = 20, step = 10),
+    sliderInput("t_drift_simulation_with_innovation", label = "t",
+                min = 50, max = 200, value = 100, step = 50),
+    sliderInput("mu_drift_simulation_with_innovation", label = "μ",
+                min = 0, max = 0.1, value = 0.01, step = 0.01),
+    actionButton("run_button_drift_simulation_with_innovation", "Run simulation")
   ),
   plotOutput("simplot")
 )
