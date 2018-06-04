c94d03c8beb258f5f94c35082f0136d3fa02c454
nevrome <clemens@nevrome.de>
2018-06-04 14:37:35 +0200

    found a way to export visNetwork to html and then to png

--- BART_2018_06_05/networkvis.R
+++ BART_2018_06_05/networkvis.R
@@ -1,5 +1,21 @@
 library(visNetwork)
 
+saveWidgetFix <- function (widget,file,...) {
+  ## A wrapper to saveWidget which compensates for arguable BUG in
+  ## saveWidget which requires `file` to be in current working
+  ## directory.
+  wd<-getwd()
+  on.exit(setwd(wd))
+  outDir<-dirname(file)
+  file<-basename(file)
+  setwd(outDir);
+  htmlwidgets::saveWidget(widget,file=file,...)
+}
+
+visSaveFix <- function (graph, file, selfcontained = TRUE, background = "white") {
+  saveWidgetFix(graph, file, selfcontained = selfcontained, background = background)
+}
+
 ## Populationsgraph: Individuen
 
 palette <- colorRampPalette(colors=c("#000000", "#FFFFFF"))
@@ -16,7 +32,8 @@ nodes0 <- tibble::tibble(
 edges0 <- tibble::tibble()
 
 visNetwork(nodes0, edges0) %>%
-  visLayout(randomSeed = 23)
+  visLayout(randomSeed = 23) %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/01.html", background = "#f1f1f1")
 
 ## Populationsgraph: Reproduktion
 
@@ -29,7 +46,8 @@ edges1 <- tibble::tibble(
 )
 
 visNetwork(nodes0, edges1) %>%
-  visLayout(randomSeed = 23)
+  visLayout(randomSeed = 23) %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/02.html", background = "#f1f1f1")
 
 ## Populationsgraph: Vertikale Beziehungen
 
@@ -44,7 +62,8 @@ edges2 <- edges1 %>% rbind(
 )
 
 visNetwork(nodes0, edges2) %>%
-  visHierarchicalLayout() 
+  visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/03.html", background = "#f1f1f1")
 
 ## Populationsgraph: Horizontale Beziehungen
 
@@ -61,7 +80,8 @@ additional_relations_small <- tibble::tibble(
 edges4 <- edges2 %>% rbind(additional_relations_small)
 
 visNetwork(nodes0, edges4) %>%
-  visHierarchicalLayout()
+  visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/04.html", background = "#f1f1f1")
 
 ## Populationsgraph: Vererbung und Transmission
 
@@ -70,35 +90,57 @@ nodes2 <- nodes0
 #1
 nodes2$color.background[c(1)] <- "orange"
 nodes2$color.background[c(12)] <- "blue"
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% visSaveFix(file = "BART_2018_06_05/network_html/01.html")
-
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/05.html", background = "#f1f1f1")
 
 #2 
 nodes2$color.background[c(2, 6)] <- "orange"
 nodes2$color.background[c(11, 15, 14)] <- "blue"
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/06.html", background = "#f1f1f1")
 
 #3
 nodes2 <- nodes2[-c(1, 12), ]
 nodes2[nodes2$id %in% c(3, 8, 9, 10), ]$color.background <- "orange"
 nodes2[nodes2$id %in% c(13), ]$color.background <- "blue"
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/07.html", background = "#f1f1f1")
 
 #4
 nodes2 <- nodes2[!(nodes2$id %in% c(2, 6, 11, 15, 14)), ]
 nodes2[nodes2$id %in% c(16, 17, 18), ]$color.background <- "orange"
 nodes2[nodes2$id %in% c(20), ]$color.background <- "blue"
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/08.html", background = "#f1f1f1")
 
 #5
 nodes2 <- nodes2[!(nodes2$id %in% c(3, 8, 9, 10, 13)), ]
 nodes2[nodes2$id %in% c(19), ]$color.background <- "blue"
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/09.html", background = "#f1f1f1")
 
 #6
 nodes2 <- nodes2[!(nodes2$id %in% c(16, 17, 18, 20)), ]
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/10.html", background = "#f1f1f1")
 
 #7 
 nodes2 <- nodes2[!(nodes2$id %in% c(19)), ]
-visNetwork(nodes2, edges4) %>% visHierarchicalLayout() 
+visNetwork(nodes2, edges4) %>% visHierarchicalLayout() %>% 
+  visSaveFix(file = "BART_2018_06_05/network_figures/11.html", background = "#f1f1f1")
+
+
+#### transform html to png ####
+
+html_files <- list.files("BART_2018_06_05/network_figures", pattern = ".html$")
+
+for (i in 1:length(html_files)) {
+  webshot::webshot(
+    file.path("BART_2018_06_05", "network_figures", html_files[i]), 
+    file.path("BART_2018_06_05", "network_figures", paste0(formatC(i, width = 2, format = "d", flag = "0"), ".png")), 
+    delay = 1,
+    vwidth = 1000,
+    vheight = 700,
+    selector = ".vis-network"
+  )
+}
